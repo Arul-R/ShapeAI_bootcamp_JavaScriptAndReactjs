d48814673d5e88f88214ebd42ba4029702893adc
Arul-R <85393585+Arul-R@users.noreply.github.com>
2021-06-05 16:04:21 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & React.js</h1>
+      <p> This was a bootcamp on Javascript and React.js taken by Shape AI on the basics of JS, React and HTML </p>
+    </div>
+  );
+}
+export default Note;
